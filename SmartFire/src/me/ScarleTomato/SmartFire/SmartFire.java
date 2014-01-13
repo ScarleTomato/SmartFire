645438297dd661cd6d187b744cef12e93e2b2668
Mike Yarbrough <shortymack15@hotmail.com>
2014-01-12 20:57:29 -0600

    Initial Commit

---
+++ SmartFire/src/me/ScarleTomato/SmartFire/SmartFire.java
@@ -0,0 +1,8 @@
+package me.ScarleTomato.SmartFire;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SmartFire extends JavaPlugin implements Listener {
+	
+}
